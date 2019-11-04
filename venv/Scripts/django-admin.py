4fe2781e90d708631a251276bcb6bf2d12bf5724
C0mm4 <comonhuman@gmail.com>
2019-11-04 09:51:54 +0900

    gutadora

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\C0mm4\Desktop\Django_Workspace\polls_C0mm4\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
